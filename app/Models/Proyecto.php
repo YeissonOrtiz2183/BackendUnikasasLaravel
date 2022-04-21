bd9e34fbc748797d27ec3d00363eb6defc9b52bb
YeissonOrtiz2183 <yeortiz902@misena.edu.co>
2022-04-21 06:23:52 -0500

    Migraciones cotizaciones

--- app/Models/Proyecto.php
+++ app/Models/Proyecto.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Proyecto extends Model
 {
     use HasFactory;
+
+    public function proyectos(){
+        return $this->hasMany(Proyecto::class);
+    }
+
+    
 }
